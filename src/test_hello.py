51f116efffcb253423e9c0c777bf50c226e04266
nofarb <nofarb@gmail.com>
2023-06-13 12:56:34 -0500

    Update test_hello.py

--- src/test_hello.py
+++ src/test_hello.py
@@ -6,8 +6,5 @@ class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello, World!")
 
-    def test_hello_world_fails(self):
-        self.assertEqual(hello_world(), "Hello, Universe!")
-
 if __name__ == "__main__":
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-results'))
